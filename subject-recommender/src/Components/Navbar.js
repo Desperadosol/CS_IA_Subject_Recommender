f42f4f7c92d4a48dfdc6b7eb5fee5349ba463ad2
Desperadosol <igor.homichenko123@gmail.com>
2023-06-10 13:09:56 +0200

    half of the navbar

--- subject-recommender/src/Components/Navbar.js
+++ subject-recommender/src/Components/Navbar.js
@@ -1,16 +1,30 @@
+import {FaBars, FaTimes} from "react-icons/fa"
+import { useRef } from "react";
+import "../Styles/nav.css"
+
+
 function Navbar() {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
         <header>
             <h3>Logo</h3>
-            <nav>
+            <nav ref={navRef}>
                 <a href="/#">Home</a>
                 <a href="/#">My work</a>
                 <a href="/#">Blog</a>
                 <a href="/#">About me</a>
-                <button>
-                    
+                <button className="nav-btn nav-close-btn"onClick={showNavbar}>
+                    <FaTimes />
                 </button>
             </nav> 
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
         </header>
     );
 }
