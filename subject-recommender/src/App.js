01be5190d33f91b835763bcc67892ca3e940925c
Desperadosol <igor.homichenko123@gmail.com>
2023-06-12 19:26:43 +0200

    smth

--- subject-recommender/src/App.js
+++ subject-recommender/src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-
-
-function MyApp() {
+import React, {useState, useEffect} from 'react';
+import Home from './page/Home';
+import Signup from './page/Signup';
+import Login from './page/Login';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+ 
+function App() {
+ 
   return (
-    <React.Fragment>
-      <Navbar />
-    </React.Fragment>
+    <Router>
+      <div>
+        <section>                              
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/signup" element={<Signup/>}/>
+              <Route path="/login" element={<Login/>}/>
+            </Routes>                    
+        </section>
+      </div>
+    </Router>
   );
 }
-
-export default MyApp;
+ 
+export default App;
--- subject-recommender/src/firebase.js
+++ subject-recommender/src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAc5HwslYL_NOblf_v4rz1i1iQG_2RjZR0",
@@ -12,3 +13,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+export default app;
