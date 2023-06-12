a0004d97557dc2b8f179b3735ca616f4d570c655
Desperadosol <igor.homichenko123@gmail.com>
2023-06-12 19:47:05 +0200

    deleted this shit

--- subject-recommender/src/App.js
+++ subject-recommender/src/App.js
@@ -1,25 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import Home from './page/Home';
-import Signup from './page/Signup';
-import Login from './page/Login';
-import { BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router-dom';
- 
+import React from "react";
+import Navbar from "./Components/Navbar";
+
+
 function App() {
- 
   return (
-    <Router>
-      <div>
-        <section>                              
-            <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/signup" element={<Signup/>}/>
-              <Route path="/login" element={<Login/>}/>
-            </Routes>                    
-        </section>
-      </div>
-    </Router>
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
   );
 }
- 
+
 export default App;
