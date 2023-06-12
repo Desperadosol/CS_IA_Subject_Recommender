cf52596632a24e16713b3961115df450142e37ac
Desperadosol <igor.homichenko123@gmail.com>
2023-06-12 18:51:21 +0200

    firebase

---
+++ subject-recommender/src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAc5HwslYL_NOblf_v4rz1i1iQG_2RjZR0",
+  authDomain: "subject-recommender.firebaseapp.com",
+  projectId: "subject-recommender",
+  storageBucket: "subject-recommender.appspot.com",
+  messagingSenderId: "951702267821",
+  appId: "1:951702267821:web:afb482f9f5e04591d56324",
+  measurementId: "G-YLCVB6S056"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
