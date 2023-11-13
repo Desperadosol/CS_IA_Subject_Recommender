c7eb798449f25471957460af8182f85898c205e7
igor <igor.homichenko123@gmail.com>
2023-11-14 00:42:17 +0100

    clean up

--- ib-cs-ia/components/footer.js
+++ ib-cs-ia/components/footer.js
@@ -1,9 +1,8 @@
-import styles from './footer.module.css';
 import Link from 'next/link';
 
 export default function Footer() {
     return (
-    <footer className={styles.footer}>
+    <footer>
         <Link href="https://www.youtube.com/watch?v=jt9mM1jw7PE">In Paris</Link>
     </footer>
   );
