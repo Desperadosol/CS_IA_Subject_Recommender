b9824b92407ff6b6edd00efb7435fcf1aaf2b41e
igor <igor.homichenko123@gmail.com>
2023-12-12 01:19:56 +0100

    reminder to add new section

--- ib-cs-ia/pages/index.js
+++ ib-cs-ia/pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
         <main>
             <HomeSection />
             <FeatureSection />
+            {/* how it works section */}
             <TechnologiesSection />
         </main>
     );
