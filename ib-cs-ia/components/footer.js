ef6ad8878eae799c2d7c25114eeb8e70f25e65aa
igor <igor.homichenko123@gmail.com>
2023-12-15 00:56:27 +0100

    footer

--- ib-cs-ia/components/footer.js
+++ ib-cs-ia/components/footer.js
@@ -1,9 +1,33 @@
 import Link from 'next/link';
 
 export default function Footer() {
-    return (
-    <footer>
-        <Link href="https://www.youtube.com/watch?v=jt9mM1jw7PE">In Paris</Link>
+  return (
+    <footer className="bg-black text-white">
+      <div className="container py-4" style={{minWidth: "90%"}}>
+        <div className="d-flex justify-content-between align-items-center">
+          <div>
+            <a href="/">
+              <img
+                src="/logo-white.svg" 
+                alt="Logo"
+                style={{ width: '120px', height: '60px' }}
+              />
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/Desperadosol" className="text-white link-offset-1 mx-3 custom-link fs-5" target="_blank"  rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.linkedin.com/in/igor-homichenko-a76b88266" className="text-white link-offset-1 custom-link fs-5" target="_blank"  rel="noopener noreferrer">LinkedIn</a>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .custom-link {
+          text-decoration: none;
+        }
+        .custom-link:hover {
+          text-decoration: underline;
+        }
+      `}</style>
     </footer>
   );
 }
